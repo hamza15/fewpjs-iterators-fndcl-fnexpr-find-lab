773563c1100381578e221258bf8d7abacb2a4415
hamza15 <sardan5@vt.edu>
2021-02-23 16:44:14 -0500

    commit tree 04a6639b492527cdc50327752ff313be31f8bad7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function superbowlWin(a) {
+    let year;
+    a.find(function(w) {
+        if (w.result === 'W'){
+            console.log(w.year)
+            year = w.year
+            return w.year
+        } 
+    })
+    return year
+}
